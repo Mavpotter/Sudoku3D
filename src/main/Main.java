96c03cff320ef60afe7ec755bd01d1d4bcd24d16
cmniclas <niclashofmann@gmx.net>
2017-11-17 09:20:47 +0100

    added main

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main
+{
+    /**
+     * The main method that starts the application.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
